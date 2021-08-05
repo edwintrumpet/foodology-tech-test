e6cfb0b6a5458b54869ba38fbc0f5bd8da0af6e9
Edwin Garcia <spark.com.co@gmail.com>
2021-08-05 15:51:43 -0500

    Set location on website

--- src/services/restaurantsService.js
+++ src/services/restaurantsService.js
@@ -1,6 +1,9 @@
+/* eslint-disable no-undef */
 const puppeteer = require('puppeteer');
 
 const rappiURL = 'https://www.rappi.com.co/restaurantes';
+const latitude = 3.4242814233739614;
+const longitude = -76.54170365914733;
 
 class RestaurantsService {
   static async getRestaurants() {
@@ -13,11 +16,31 @@ class RestaurantsService {
     };
     const browser = await puppeteer.launch(options);
     const page = await browser.newPage();
+    await page.setGeolocation({
+      latitude,
+      longitude,
+    });
     await page.goto(rappiURL);
-    // eslint-disable-next-line no-undef
-    const text = await page.evaluate(() => document.getElementsByTagName('rappi-mf-address')[0].innerText);
-    console.log(text);
-    return text;
+
+    // Set location on page
+    await page.click(document.getElementsByTagName('rappi-mf-address')[0]
+      .children[1]
+      .children[0]
+      .children[1]);
+
+    await page.click(document.getElementsByTagName('rappi-mf-address')[0]
+      .children[1]
+      .children[1]
+      .children[0]);
+
+    await page.click(document.getElementsByTagName('rappi-mf-modal-address')[0]
+      .children[1]
+      .children[0]
+      .children[1]
+      .children[2]
+      .children[0]);
+
+    return 'response';
   }
 }
 
